3271bffda7504ae4f470f968bf926fc5f76543b3
Marc Le Masson <sylaratomic@gmail.com>
2018-07-03 11:47:23 +0200

    Challenge 1: Zombie Game! Pt3 - Fix the wall collision machine gun problem. Much smoother now.

--- ZombieGame/Agent.cpp
+++ ZombieGame/Agent.cpp
@@ -1,6 +1,7 @@
 #include "Agent.h"
 #include <Bengine/ResourceManager.h>
 #include "Level.h"
+#include <algorithm>
 
 
 Agent::Agent()
@@ -80,21 +81,25 @@ void Agent::collideWithTile(glm::vec2 tilePos){
 	const float TILE_RADIUS = (float)TILE_WIDTH / 2.0f;
 	const float MIN_DISTANCE = AGENT_RADIUS + TILE_RADIUS;
 
-	glm::vec2 distVec = _position - tilePos;
+	glm::vec2 centerPlayerPos = _position + glm::vec2(AGENT_RADIUS);
+	glm::vec2 distVec = centerPlayerPos - tilePos;
 
-	float xDepth = MIN_DISTANCE - distVec.x;
-	float yDepth = MIN_DISTANCE - distVec.y;
+	float xDepth = MIN_DISTANCE - abs(distVec.x);
+	float yDepth = MIN_DISTANCE - abs(distVec.y);
 
-	float absXDepth = abs(xDepth);
-	float absYDepth = abs(yDepth);
-
-
-	if (xDepth > 0 || yDepth > 0) {
-		if (absXDepth < absYDepth) {
-			_position += xDepth;
+	if (std::max(xDepth, 0.0f) < std::max(yDepth, 0.0f)) {
+		if (distVec.x < 0) {
+			_position.x -= xDepth;
 		}
 		else {
-			_position += yDepth;
+			_position.x += xDepth;
 		}
-	}
+	}	else {
+		if (distVec.y < 0) {
+			_position.y -= yDepth;
+	}	else {
+			_position.y += yDepth;
+			}
+		}
+
 }
